8b602a35125849bee84613e76100f9bb8a1ab7c5
Olivier Anani <ananiolivier44@gmail.com>
2026-03-04 10:55:32 +0000

    Mise à jour manuelle complète

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,3 @@
-/**
- * service-worker.js - Gère le cache et les requêtes réseau pour la PWA
- * Stratégie : cache-first pour les assets statiques, réseau pour data.json
- */
-
 const CACHE_NAME = 'mr-xpronos-v1';
 const urlsToCache = [
     '/',
@@ -30,72 +25,38 @@ const urlsToCache = [
     '/assets/images/instagram.png',
     '/assets/images/facebook.png',
     '/assets/images/youtube.png',
-    '/assets/images/phone.png'
+    '/assets/images/phone.png',
+    '/assets/images/icon-192.png',
+    '/assets/images/icon-512.png'
 ];
 
-// Installation : mise en cache des fichiers statiques
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Mise en cache des ressources statiques');
-                return cache.addAll(urlsToCache);
-            })
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
 });
 
-// Activation : nettoyage des anciens caches
 self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.filter(name => name !== CACHE_NAME)
-                    .map(name => caches.delete(name))
-            );
-        })
+        caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
     );
 });
 
-// Interception des requêtes : stratégie cache-first pour les statiques, réseau pour data.json
 self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
-
-    // Pour data.json, toujours passer par le réseau (pour avoir les scores à jour)
     if (url.pathname.endsWith('/data.json')) {
         event.respondWith(
             fetch(event.request)
-                .then(response => {
-                    // Mettre en cache la nouvelle version
-                    const responseClone = response.clone();
-                    caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, responseClone);
-                    });
-                    return response;
+                .then(res => {
+                    const clone = res.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                    return res;
                 })
-                .catch(() => {
-                    // Si réseau indisponible, essayer le cache
-                    return caches.match(event.request);
-                })
+                .catch(() => caches.match(event.request))
         );
-        return;
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(res => res || fetch(event.request))
+        );
     }
-
-    // Pour les autres ressources (CSS, JS, images, HTML), on utilise cache-first
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response; // retourne la version en cache
-                }
-                // Sinon, on va chercher sur le réseau
-                return fetch(event.request).then(networkResponse => {
-                    // Mettre en cache la nouvelle ressource
-                    const responseClone = networkResponse.clone();
-                    caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, responseClone);
-                    });
-                    return networkResponse;
-                });
-            })
-    );
 });
